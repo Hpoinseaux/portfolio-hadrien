2c5085fa3827ae1d4fcac497f3041e79e6fb273f
Hadrien Poinseaux <hadrien.poinseaux@gmail.com>
2025-05-22 15:57:29 +0200

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
-import content from '@astrojs/content';
 
 export default defineConfig({
-  integrations: [tailwind(), mdx(), content()],
+  integrations: [tailwind(), mdx()],
 });
